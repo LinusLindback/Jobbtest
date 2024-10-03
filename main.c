901d7968338843e8673ccd4a390c2729e4fe0b63
LinusLindback <llinuslindback@gmail.com>
2024-10-03 13:19:33 +0200

    Added main.c to repository

---
+++ main.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int print_hello_world() {
+    printf("Hello, World! This is an updated version.\n");  // Uppdaterad utskrift
+    return 0;
+}
+
+int main(int argc, const char * argv[]) {
+    return print_hello_world();
+}
