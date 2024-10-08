8878ae11781ad470ec0c810fcc3b11adfc067658
LinusLindback <llinuslindback@gmail.com>
2024-10-08 20:40:27 +0200

    456

--- main.c
+++ main.c
@@ -1,11 +1,8 @@
-
 #include <stdio.h>
 
-int print_hello_world() {
-    printf("Hello, World! This is an updated version.\n");  // Uppdaterad utskrift
+int main() {
+    // Print Hello, World! to the console
+    printf("Hello, World!\n");
     return 0;
 }
 
-int main(int argc, const char * argv[]) {
-    return print_hello_world();
-}
